111a8e67aa1dd10d32ccf5f72998d140d13a3665
Eupou <wesleywesleygomesgomes@gmail.com>
2021-11-03 23:04:11 -0300

    Melhorando a lógica da busca e add mais options

--- main.js
+++ main.js
@@ -1,36 +1,51 @@
-let listCores = document.querySelectorAll('#list-cores li')
+let listItems = document.querySelectorAll('#list-cores li')
+let todasUl = document.querySelectorAll('ul')
 let textList = []
-let coresValidas = []
+let optionsValidas = []
 
-listCores.forEach(element => {
+const filtraOption = () =>  {
+    let select = document.getElementById('select-opcoes')
+    let option = select.options[select.selectedIndex]
+    mostraOption(option.value)
+}
+
+const mostraOption = (option)=> {
+    todasUl.forEach(ul => {
+        if(ul.className != `list-${option} list-global`) {
+            ul.style.display = 'none'
+        } else {
+            ul.style.display = 'flex'
+        }
+    });
+}
+
+filtraOption()
+
+listItems.forEach(element => {
     textList.push(element.textContent)
 });
 
 const BUSCA_FRASE = () => {
     let frase = document.querySelector('#texto-cores')
-    let corRetornada = FILTRA_ITEMS(frase.value)
-
-    while(corRetornada.length > 0) {
-        let cor = corRetornada.pop()
-        
-        if (cor.indexOf(frase.value) == 0) {
-            coresValidas.push(cor)
-        }
+    let optionRetornada = FILTRA_ITEMS(frase.value)
+    while(optionRetornada.length > 0) {
+        let option = optionRetornada.pop()
+        option.toLowerCase(optionsValidas.push(option))
     }
     
     RESETA_ORDER()
 }
 
 const RESETA_ORDER = () => {
-    listCores.forEach(element => {
+    listItems.forEach(element => {
         element.style.order = 1
     });
 
-    while (coresValidas.length > 0) {
-        let cor = coresValidas.pop()
+    while (optionsValidas.length > 0) {
+        let item = optionsValidas.pop()
+        let nDaList = textList.indexOf(item.toString())
 
-        let nDaList = textList.indexOf(cor.toString())
-        listCores[nDaList].style.order = 0
+        listItems[nDaList].style.order = 0
     }
 }
 
